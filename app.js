327d45a414f0b72786a183456956c571f1cc88e5
Stefan Koev <st.koev@outlook.com>
2022-09-20 22:36:53 +0300

    refactor constructor function to class, final commit

--- app.js
+++ app.js
@@ -4,37 +4,38 @@ function getElement(selection) {
   throw new Error(`Please check ${selection} selector, no such element.`);
 }
 
-function Counter(element, value) {
-  this.counter = element;
-  this.value = value;
-  this.increaseBtn = this.counter.querySelector(".increase");
-  this.decreaseBtn = this.counter.querySelector(".decrease");
-  this.resetBtn = this.counter.querySelector(".reset");
-  this.valueDOM = this.counter.querySelector(".value");
-  this.valueDOM.textContent = this.value;
+class Counter {
+  constructor(element, value) {
+    this.counter = element;
+    this.value = value;
+    this.increaseBtn = this.counter.querySelector(".increase");
+    this.decreaseBtn = this.counter.querySelector(".decrease");
+    this.resetBtn = this.counter.querySelector(".reset");
+    this.valueDOM = this.counter.querySelector(".value");
+    this.valueDOM.textContent = this.value;
 
-  //   bind this to all functions
-  this.increase = this.increase.bind(this);
-  this.decrease = this.decrease.bind(this);
-  this.reset = this.reset.bind(this);
-  this.increaseBtn.addEventListener("click", this.increase);
-  this.decreaseBtn.addEventListener("click", this.decrease);
-  this.resetBtn.addEventListener("click", this.reset);
+    //   bind this to all functions
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset = this.reset.bind(this);
+    this.increaseBtn.addEventListener("click", this.increase);
+    this.decreaseBtn.addEventListener("click", this.decrease);
+    this.resetBtn.addEventListener("click", this.reset);
+  }
+  increase = function () {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  };
+  decrease = function () {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  };
+  reset = function () {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+  };
 }
 
-Counter.prototype.increase = function () {
-  this.value++;
-  this.valueDOM.textContent = this.value;
-};
-Counter.prototype.decrease = function () {
-  this.value--;
-  this.valueDOM.textContent = this.value;
-};
-Counter.prototype.reset = function () {
-  this.value = 0;
-  this.valueDOM.textContent = this.value;
-};
-
-const firstCounter = new Counter(getElement(".first-counter"), 100);
+const firstCounter = new Counter(getElement(".first-counter"), 1400);
 
 const secondCounter = new Counter(getElement(".second-counter"), 300);
