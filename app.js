8f395b4d789f23bb44a610c30a20a4bd21fa819a
Stefan Koev <st.koev@outlook.com>
2022-09-20 22:29:54 +0300

    implement increase, decrease, reset buttons

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(`Please check ${selection} selector, no such element.`);
+}
+
 function Counter(element, value) {
   this.counter = element;
   this.value = value;
@@ -7,18 +13,28 @@ function Counter(element, value) {
   this.valueDOM = this.counter.querySelector(".value");
   this.valueDOM.textContent = this.value;
 
-  this.increaseBtn.addEventListener("click", () => {
-    this.value++;
-    this.valueDOM.textContent = this.value;
-  });
+  //   bind this to all functions
+  this.increase = this.increase.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.reset = this.reset.bind(this);
+  this.increaseBtn.addEventListener("click", this.increase);
+  this.decreaseBtn.addEventListener("click", this.decrease);
+  this.resetBtn.addEventListener("click", this.reset);
 }
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
 
 const firstCounter = new Counter(getElement(".first-counter"), 100);
 
 const secondCounter = new Counter(getElement(".second-counter"), 300);
-
-function getElement(selection) {
-  const element = document.querySelector(selection);
-  if (element) return element;
-  throw new Error(`Please check ${selection} selector, no such element.`);
-}
